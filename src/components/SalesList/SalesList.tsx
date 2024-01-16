811cf467a8c2cd5f2508ebeb81c5abd70a0f5065
Bryan Oliveira <bsantos.cpp@gmail.com>
2024-01-16 16:59:51 -0300

    removing collapse

--- src/components/SalesList/SalesList.tsx
+++ src/components/SalesList/SalesList.tsx
@@ -55,14 +55,11 @@ export default function SalesList() {
                     <div className="card-body">
                         <h5 className="card-title d-flex">
                             <b>R$ {sale.totalPrice?.toFixed(2)}</b>
-                            <a className="d-flex ms-auto text-decoration-none fs-6" data-bs-toggle="collapse" href={`#sale-${sale.id}-itens`} role="button" aria-expanded="false" aria-controls={`#sale-${sale.id}-itens`}>
-                                <i className="bi bi-caret-down-fill"></i> 
-                            </a>
                         </h5>
                         <h6 className="card-subtitle text-secondary">
                             {sale.paymentMethod}
                         </h6>
-                        <p className="card-text mt-2 collapse" id={`sale-${sale.id}-itens`}>
+                        <p className="card-text mt-2" id={`sale-${sale.id}-itens`}>
                             <span> {sale.description} </span>
                         </p>
                     </div>
