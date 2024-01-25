a994c48412d6928898abaa6d2307c09c8a4c1d92
Bryan Oliveira <bsoliveira@proton.me>
2024-01-25 15:32:47 -0300

    add protection on route by accessToken

--- src/main.tsx
+++ src/main.tsx
@@ -7,29 +7,26 @@ import Register from './pages/Register/Register.tsx'
 import Home from './pages/Home/Home.tsx'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
+const isLogged = localStorage.getItem('accessToken')
+
 const router = createBrowserRouter([
   {
     path: '',
-    element: <Login/>
+    element: isLogged ? <Home/> : <Login/>
   },
   {
     path: 'login',
-    element: <Login/>
+    element: <Login/>,
   },
   {
     path: 'register',
     element: <Register/>
-  },
-  {
-    path: 'home',
-    element: <Home/>
   }
 ])
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router}/>
